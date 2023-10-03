67baa89931d1409e8f3329f27ef119f5739d620c
unknown <deepok626@gmail.com>
2023-10-03 11:17:52 +0530

    event and HTTP module

---
+++ ch12/index.js
@@ -0,0 +1,25 @@
+// create web server - http Mudule
+import http from 'http';
+
+const server = http.createServer((req,res)=>{
+        res.setHeader('Content-Type', 'text/plain');
+        res.end("Response from server")
+})
+
+server.listen(8000,"localhost",()=>{
+    console.log("server running at http://localhost:8000")
+});
+
+
+
+// const server = http.createServer((req,res)=>{
+//     res.setHeader('Content-Type', 'text/plain');
+//     res.end("Response from server 1")
+// })
+
+// const PORT = process.env.PORT || 8000;
+// const HOST = 'localhost'
+
+// server.listen(PORT,HOST,()=>{
+// console.log("server running at http://localhost:8000")
+// });
